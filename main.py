9d1709175ba88b26241c614aa5105b267182396e
kethavath raja nayak <cse.raja007@gmail.com>
2026-01-15 23:09:00 +0530

    second commit

--- main.py
+++ main.py
@@ -12,10 +12,15 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm,RegisterForm,Loginform,CommentForm
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # loads .env file
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -40,7 +45,11 @@ def load_user(user_id):
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DB_URI',
+    'sqlite:///posts.db'
+)
+
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -263,4 +272,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False)
